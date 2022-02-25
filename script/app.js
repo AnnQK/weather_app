258663b2828f2f53a1fe8d7375a3db17698cc85a
AnnQK <annhuddy13@yandex.ru>
2022-02-25 19:14:39 +0300

    feat: add ui class for dom manipulations

--- script/app.js
+++ script/app.js
@@ -9,6 +9,28 @@ const tempMetric = document.getElementById("metric");
 const daytimeImage = document.querySelector(".container__info-image");
 const key = "KEiGR6Gd0rodktHWaPWghgxFESq6usGO";
 
+class UI {
+  static updateCity(data) {
+    cityInfo.textContent = data[0].LocalizedName;
+  }
+
+  static updateCondition(data) {
+    conditionInfo.textContent = data[0].WeatherText;
+  }
+
+  static updateTemp(data) {
+    tempInfo.textContent = data[0].Temperature.Metric.Value;
+  }
+
+  static updateDayTime(data) {
+    data[0].IsDayTime
+      ? (daytimeImage.style.background =
+          '#fff url("../../assets/img/logo/day.png") no-repeat center')
+      : (daytimeImage.style.background =
+          '#495057 url("../../assets/img/logo/night.png") no-repeat center');
+  }
+}
+
 async function getCity(city) {
   const response = await fetch(
     `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`
