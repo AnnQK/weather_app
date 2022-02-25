b1121c36b1f245e1e19c1a861ff9179b0193eab1
AnnQK <annhuddy13@yandex.ru>
2022-02-25 19:13:28 +0300

    feat: add fucntions to get data from api

--- script/app.js
+++ script/app.js
@@ -8,3 +8,21 @@ const tempInfo = document.getElementById("temp-info");
 const tempMetric = document.getElementById("metric");
 const daytimeImage = document.querySelector(".container__info-image");
 const key = "KEiGR6Gd0rodktHWaPWghgxFESq6usGO";
+
+async function getCity(city) {
+  const response = await fetch(
+    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+async function displayCondition(preData) {
+  const cityCode = preData[0].Key;
+  const response = await fetch(
+    `http://dataservice.accuweather.com/currentconditions/v1/${cityCode}?apikey=${key}`
+  );
+  const data = await response.json();
+  return data;
+}
