2c6e762660e09bacb24fcf4043e7dd39eaba1193
AnnQK <annhuddy13@yandex.ru>
2022-02-25 19:07:00 +0300

    feat: add variables for script

---
+++ script/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const cityInput = document.getElementById("city");
+const submitBtn = document.querySelector(".btn");
+const cityInfo = document.getElementById("city-info");
+const conditionInfo = document.getElementById("condition-info");
+const tempInfo = document.getElementById("temp-info");
+const tempMetric = document.getElementById("metric");
+const daytimeImage = document.querySelector(".container__info-image");
+const key = "KEiGR6Gd0rodktHWaPWghgxFESq6usGO";
